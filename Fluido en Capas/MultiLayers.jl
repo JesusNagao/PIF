a82b813f3da26d033b4fd27efba9624f3f29f203
JesusNagao <98624459+JesusNagao@users.noreply.github.com>
2022-02-28 22:25:09 -0600

    Update MultiLayers.jl

--- Fluido en Capas/MultiLayers.jl
+++ Fluido en Capas/MultiLayers.jl
@@ -1,7 +1,7 @@
 using Plots;
 include("MultiLayerOperation.jl")
 
-n = 1 #Number of Layers
+n = 5 #Number of Layers
 
 x = Array{Float64}([i for i in range(0.0, stop=1000.0, step=10)]);
 y = Array{Float64}(undef, length(x));
@@ -15,10 +15,9 @@ u[length(x)] = 0;
 const dx = 10.0;
 const g = 9.8;
 const h0 = 10.0;
-const dt = 0.1;
-it = 10;
+const dt = 0.01;
+it = 500;
 
 gaussian(x, y, h, 500.0, 750.0)
 init_Layers(layer, y, u, h)
-#animate(x, layer, it, g, dx, dt, 0.05, h0, n)
-
+@time animate(x, layer, it, g, dx, dt, 0.05, h0, n)
